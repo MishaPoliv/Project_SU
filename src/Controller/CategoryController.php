39eb4a1a28946fc098bcd51b7fc93437eb616bc7
MishaPoliv <xFun777@gmail.com>
2018-04-03 21:25:53 +0300

    Lesson 17

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -12,18 +12,47 @@ class CategoryController extends Controller
     public function index()
     {
         $repo = $this->getDoctrine()->getRepository(Category::class);
-        $categories = $repo->findAll();
+       // $categories = $repo->findBy(['parent' => null]); //<< вместо, следующее:
+
+        $qb = $repo->createQueryBuilder('cat');
+        $qb
+            ->select('cat')
+            ->where('cat.parent IS NULL')
+
+        ;
+
+        $categories = $qb->getQuery()->execute();
+
         return $this->render('category/index.html.twig', [
             'categories'=>$categories
         ]);
     }
+
+
     /**
      * @Route("/category/{id}", name="category_show")
      */
-    public function showAction(Category $category)
+    public function show($id)
     {
+        $repo = $this->getDoctrine()->getRepository(Category::class);
+
+        $qb = $repo->createQueryBuilder('cat');
+        $qb
+            ->leftJoin('cat.subcategories', 'subcat')
+            ->leftJoin('cat.products', 'p')
+            ->select('cat, subcat, p')
+            ->where('cat.id = :id')
+            ->setParameter('id', $id);
+
+        $category = $qb->getQuery()->getOneOrNullResult();
+
+        if (!$category) {
+
+            throw $this->createNotFoundException('Category with id#'.$id.' not found.');
+
+        }
         return $this->render('category/show.html.twig', [
-            'category' => $category,
+                      'category' => $category,
         ]);
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -1,10 +1,6 @@
 <?php
-
 namespace App\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Validation\Category;
-
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
  * @ORM\Table(name="products")
@@ -17,78 +13,70 @@ class Product
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $title;
-
     /**
      * @ORM\Column(type="decimal", precision=10, scale=2, nullable=true)
      */
     private $price;
-
     /**
      * @ORM\Column(type="text", nullable=true)
      */
-    private $descripiton;
-
+    private $description;
     /**
      * @var Category
      *
-     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="products")
      * @ORM\JoinColumn(nullable=true)
      */
     private $category;
-
     public function getId()
     {
         return $this->id;
     }
-
     public function getTitle(): ?string
     {
         return $this->title;
     }
-
     public function setTitle(string $title): self
     {
         $this->title = $title;
-
         return $this;
     }
-
     public function getPrice()
     {
         return $this->price;
     }
-
     public function setPrice($price): self
     {
         $this->price = $price;
-
         return $this;
     }
-
     /**
      * @return mixed
      */
-    public function getDescripiton()
+    public function getDescription()
     {
-        return $this->descripiton;
+        return $this->description;
     }
-
     /**
-     * @param mixed $descripiton
-     *
+     * @param mixed $description
      * @return Product
      */
-    public function setDescripiton($descripiton): self
+    public function setDescription($description):self
     {
-        $this->descripiton = $descripiton;
-
+        $this->description = $description;
         return $this;
     }
-
-
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+        return $this;
+    }
 }
