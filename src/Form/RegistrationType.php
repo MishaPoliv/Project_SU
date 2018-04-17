e8b1c07523d25d04fb91e488e02838bd5398d8ca
MishaPoliv <xfun777@gmail.com>
2018-04-17 21:38:42 +0300

    les 20

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skillup
+ * Date: 17.04.18
+ * Time: 20:59
+ */
+
+namespace App\Form;
+
+
+use FOS\UserBundle\Form\Type\RegistrationFormType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('acceptRules', CheckboxType::class);
+    }
+
+
+    public function getParent()
+    {
+        return RegistrationFormType::class;
+    }
+
+}
